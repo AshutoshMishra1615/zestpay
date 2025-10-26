f009a790971672e6f89f77cca2985e41a6591990
tabrez <04tabrez@gmail.com>
2025-10-26 12:05:43 +0530

    minor fix

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDzmMBH4VaHskX6-Tc-8vxkchok25XGGPQ",
+  authDomain: "innovathon-d112f.firebaseapp.com",
+  projectId: "innovathon-d112f",
+  storageBucket: "innovathon-d112f.firebasestorage.app",
+  messagingSenderId: "899428158073",
+  appId: "1:899428158073:web:1c47d39bc5f17e380ae2a8",
+  measurementId: "G-KF4GHBYX7M"
 };
 
 const app = initializeApp(firebaseConfig);
